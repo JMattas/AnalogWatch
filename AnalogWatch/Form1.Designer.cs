77c034ee0e63f9037e68ed9f9a22e096c48335f1
JMattas <jan.mattas@seznam.cz>
2023-02-28 23:39:26 +0100

    Add project files.

---
+++ AnalogWatch/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace AnalogWatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.Location = new System.Drawing.Point(61, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(250, 250);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(157, 289);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(178, 289);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(198, 289);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(376, 336);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ AnalogWatch/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
+
+namespace AnalogWatch
+{
+
+    public partial class Form1 : Form
+    {
+        Point startingPoint = new Point(100, 100);
+        float seconds;
+        float minutes;
+        float hours;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           eraseHands();
+            updateTime();
+           refreshStuff();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            Application.DoEvents();
+            
+            updateTime();
+            timer1.Start();
+            drawClock();
+            refreshStuff();
+            
+        }
+        private void updateTime()
+        {
+            var CurDate = DateTime.Now;
+            label1.Text = "" + CurDate.Hour + " :";
+            label2.Text = "" + CurDate.Minute + " :";
+            label3.Text = "" + CurDate.Second;
+            hours = CurDate.Hour%12;
+            minutes = CurDate.Minute - 15;
+            seconds = CurDate.Second - 15;
+        }
+        void refreshStuff()
+        {
+            updateTime();
+            drawHands();
+        }
+        void drawClock()
+        {
+            var g = pictureBox1.CreateGraphics();
+            Pen clock = new Pen(Color.Black, 2);
+            g.DrawEllipse(clock, 25, 25, 200, 200);
+
+ 
+            Pen smallLines = new Pen(Color.Black, 1);
+            Brush letterBrush = new SolidBrush(Color.Black);
+           
+            for (int i = 1; i <= 60; i++)
+            {
+                g.TranslateTransform(125, 125);
+               
+                g.RotateTransform(i * 6);
+                
+                if (i % 5==0)
+                {
+                    
+                    g.DrawLine(smallLines, 75, 0, 100, 0);
+
+                }
+                else
+                {
+                    g.DrawLine(smallLines, 90, 0, 100, 0);
+                }
+
+                
+                g.ResetTransform();
+            }
+            Font font = new Font("Arial", 12);
+            StringFormat format = new StringFormat();
+            format.Alignment = StringAlignment.Center;
+            format.LineAlignment = StringAlignment.Center;
+            
+            for (int i = 1; i <= 12; i++)
+            {
+                g.TranslateTransform(125, 125);
+                g.RotateTransform(i * 30);
+                g.DrawString(i.ToString(), font, letterBrush, new PointF(0, -110),format);
+                g.ResetTransform();
+            }
+            
+           
+        }
+        void eraseHands()
+        {
+            var g = pictureBox1.CreateGraphics();
+            Pen secondHand = new Pen(Color.White, 1);
+            g.TranslateTransform(125, 125);
+            g.RotateTransform((seconds) * 6);
+            g.DrawLine(secondHand, 0, 0, 80, 0);
+            g.ResetTransform();
+
+            Pen minuteHand = new Pen(Color.White, 2);
+            g.TranslateTransform(125, 125);
+            g.RotateTransform(minutes * 6);
+            g.DrawLine(minuteHand, 0, 0, 65, 0);
+            g.ResetTransform();
+
+            Pen hourHand = new Pen(Color.White, 3);
+            g.TranslateTransform(125, 125);
+            g.RotateTransform((hours+ (minutes + 15) / 60) * 30 - 90);
+            g.DrawLine(hourHand, 0, 0, 55, 0);
+            g.ResetTransform();
+        }
+        void drawHands()
+        {
+            var g = pictureBox1.CreateGraphics();
+
+            Pen minuteHand = new Pen(Color.Black, 2);
+            g.TranslateTransform(125, 125);
+            g.RotateTransform(minutes * 6);
+            g.DrawLine(minuteHand, 0, 0, 65, 0);
+            g.ResetTransform();
+
+            Pen hourHand = new Pen(Color.Black, 3);
+            g.TranslateTransform(125, 125);
+            g.RotateTransform((hours + (minutes + 15)/60) * 30 -90);
+            g.DrawLine(hourHand, 0, 0, 55, 0);
+            g.ResetTransform();
+
+            Pen secondHand = new Pen(Color.Red, 1);
+            g.TranslateTransform(125, 125);
+            g.RotateTransform((seconds) * 6);
+            g.DrawLine(secondHand, 0, 0, 80, 0);
+            g.ResetTransform();
+
+        }
+
+    }
+}
